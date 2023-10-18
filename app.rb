e7f2c04025bf8f2dfe4babcc80abc2b091cfb306
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-10-18 14:07:48 +0000

    working flexible.erb and app.rb with dynamic dice

--- app.rb
+++ app.rb
@@ -52,3 +52,19 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+
+get("/dice/:zebra/:ox") do
+
+  @number_of_dice = params.fetch("zebra").to_i
+  @number_of_sides = params.fetch("ox").to_i
+  
+  @rolls = []
+
+  @number_of_dice.times do
+  die = rand(1..@number_of_sides)
+
+  @rolls.push(die)
+end
+
+erb(:flexible)
+end
